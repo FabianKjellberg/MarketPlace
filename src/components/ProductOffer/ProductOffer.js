cc06c843210fa9f2c041800d2e2de481e332c7bf
Arturo Bendezu <artycok123@gmail.com>
2024-05-19 20:41:51 +0200

    Function for the buttons accept and reject offer  8yet to properly be implemented)

--- src/components/ProductOffer/ProductOffer.js
+++ src/components/ProductOffer/ProductOffer.js
@@ -2,14 +2,40 @@ import ProductListing from '../ProductListing/ProductListing';
 import './ProductOffer.css'
 import React, { useEffect } from 'react';
 import { useState } from 'react';
+import { useAuthentication } from '../../utilities/AuthenticationProvider.js';
+import axios from "axios";
+import AcceptProductManager from '../../utilities/AcceptProductManager.js';
+import RejectProductManager from '../../utilities/RejectProductManager.js';
 
 function ProductOffer(props) {
 
     const [product, setProduct] = useState(props.product)
+    const [isOfferHandled, setIsOfferHandled] = useState(false); // Add this line
+    const { token } = useAuthentication();
+
+    const acceptProductManager = new AcceptProductManager("http://localhost:8080");
+    const rejectProductManager = new RejectProductManager("http://localhost:8080");
+
 
     useEffect(() => {
         setProduct(props.product)
     }, [props.product]);
+
+    const handleAccept = (productId) => {
+        acceptProductManager.AcceptOffer(productId, token)
+        setProduct({...product, buyerUsername: ""})
+        setIsOfferHandled(true); // Add this line
+    }
+
+    const handleReject = (productId) => {
+        rejectProductManager.RejectOffer(productId, token)
+        setProduct({...product, buyerUsername: ""})
+        setIsOfferHandled(true); // Add this line
+    }
+
+    if (isOfferHandled) {
+        return null;
+    }
     
     return (
     <>
@@ -17,12 +43,13 @@ function ProductOffer(props) {
             
             <ProductListing 
                 product={product}
+                isRemovable={true}
             />
             <div className='product-offer-buttons'>
                 <p>{product.buyerUsername ? product.buyerUsername : "DEFAULT" } wants to purchase your item</p>
                 <div className='product-offer-buttons-wrapper'>
-                    <button>Accept</button>
-                    <button>Reject</button>
+                    <button onClick={handleAccept}>Accept</button>
+                    <button onClick={handleReject}>Reject</button>
                 </div> 
             </div>
         </div> 
